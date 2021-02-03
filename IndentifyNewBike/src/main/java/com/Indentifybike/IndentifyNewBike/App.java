c64c30e1ead6343d9d8f5bad7e9f93bda5244ca3
Kishore Kumar <kishorebca98@gmail.com>
2021-02-03 15:38:21 +0530

    inital

---
+++ IndentifyNewBike/src/main/java/com/Indentifybike/IndentifyNewBike/App.java
@@ -0,0 +1,13 @@
+package com.Indentifybike.IndentifyNewBike;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
